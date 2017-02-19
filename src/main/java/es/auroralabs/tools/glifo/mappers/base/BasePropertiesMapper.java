3f457c9672ee71bcefad658840d792f4a7fa0e79
Fran Soto <fran.soto@outlook.com>
2017-02-19 14:01:16 +0100

    Fix bug on toEntity

--- src/main/java/es/auroralabs/tools/glifo/mappers/base/BasePropertiesMapper.java
+++ src/main/java/es/auroralabs/tools/glifo/mappers/base/BasePropertiesMapper.java
@@ -199,7 +199,7 @@ public class BasePropertiesMapper<E extends ModelEntity, M extends ModelData> {
                             Class dumpedClass = Class.forName(modelName);
                             MapperComponent mapperComponent = getMapperComponentFromName(mapperNameFromModelName(dumpedClass.getName()));
                             if (mapperComponent != null) {
-                                Method setterEntityMethod = entityClass.getMethod(modelMapper.get(getter), entityClass);
+                                Method setterEntityMethod = entityClass.getMethod(modelMapper.get(getter), modelReturnClass);
 
                                 Method mapperToEntityMethod = null;
                                 try {
@@ -450,7 +450,7 @@ public class BasePropertiesMapper<E extends ModelEntity, M extends ModelData> {
     }
 
     private String mapperNameFromModelName(String entityName) {
-        return entityName.replace("Data", "Mapper").replace(".datasource.model.", ".domain.mappers.");
+        return entityName.replace("Data", "Mapper").replace(".model.", ".mappers.");
     }
 
     private String cleanTypeName(String typeName) {
